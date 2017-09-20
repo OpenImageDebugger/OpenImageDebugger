ab0b5322a7c543a9f608c1a66f3b1ca3f1b061f3
Claudio dos Santos Fernandes <claudius@ufmg.br>
2017-09-19 22:53:34 -0300

    Refactor shaders module

--- src/visualization/shaders/text_fs.cpp
+++ src/visualization/shaders/text_fs.cpp
@@ -1,4 +1,30 @@
-namespace shader {
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015-2017 GDB ImageWatch contributors
+ * (github.com/csantosbh/gdb-imagewatch/)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+namespace shader
+{
 
 const char* text_frag_shader = R"(
 
@@ -10,8 +36,8 @@ uniform vec4 brightness_contrast[2];
 // Ouput data
 varying vec2 uv;
 
-float roundFloat(float f) {
-    return float(int(f+0.5));
+float round_float(float f) {
+    return float(int(f + 0.5));
 }
 
 void main()
@@ -19,10 +45,11 @@ void main()
     vec4 color;
     // Output color = red
     float buff_color = texture2D(buff_sampler, pix_coord).r;
-    buff_color = buff_color*brightness_contrast[0].x + brightness_contrast[1].x;
+    buff_color = buff_color * brightness_contrast[0].x +
+                              brightness_contrast[1].x;
 
     float text_color = texture2D(text_sampler, uv).r;
-    float pix_intensity = roundFloat(1.0-buff_color);
+    float pix_intensity = round_float(1.0-buff_color);
 
     color = vec4(vec3(pix_intensity), text_color);
 
