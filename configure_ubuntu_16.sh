90d188b766dd01037f408b342c409c37babadfc0
Solo Super <ruffens@gmail.com>
2017-11-21 08:46:46 +0100

    Update installation script

--- configure_ubuntu_16.sh
+++ configure_ubuntu_16.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 echo "Starting GDB ImageWatch configuration for Ubuntu 16.04."
 set -e
-sudo apt-get install python3-pip
-sudo apt-get -y install libpython3-dev libglew-dev python3-numpy python3-pip texinfo libfreetype6-dev libeigen3-dev
-sudo pip3 install pysigset
-mkdir build
+sudo apt update
+sudo apt install -y python3-pip
+sudo apt install -y libpython3-dev libglew-dev python3-numpy python3-pip texinfo libfreetype6-dev libeigen3-dev
+sudo -H pip3 install pysigset
+mkdir -p build
 cd build
 export QT_SELECT=5
 qmake ..
 make -j$(nproc)
-make install
+sudo make install
 cd ..
 echo "source $(pwd)/build/gdb-imagewatch.py" > ~/.gdbinit
 echo "Completed configuration of GDB ImageWatch."
