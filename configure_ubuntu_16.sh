f597f8adbc63070aca82b4d4e999f0754189b3df
Solo Super <ruffens@gmail.com>
2017-11-30 09:37:52 +0100

    remove duplicate 'install python3-pip'

--- configure_ubuntu_16.sh
+++ configure_ubuntu_16.sh
@@ -2,7 +2,6 @@
 echo "Starting GDB ImageWatch configuration for Ubuntu 16.04."
 set -e
 sudo apt update
-sudo apt install -y python3-pip
 sudo apt install -y libpython3-dev libglew-dev python3-numpy python3-pip texinfo libfreetype6-dev libeigen3-dev
 sudo -H pip3 install pysigset
 mkdir -p build
