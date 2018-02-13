e0b51c0f3b9a698d482f62149531cce65ac0be6c
Claudio dos Santos Fernandes <claudius@ufmg.br>
2018-02-13 19:55:29 -0200

    Perform submodule initialization on setup script

--- configure_ubuntu_16.sh
+++ configure_ubuntu_16.sh
@@ -2,6 +2,8 @@
 echo -e "\tThis version of GDB ImageWatch requires Qt 5.6 (or more recent)."
 echo -e "\tIf you don't have it, please download it from https://www.qt.io/download"
 echo "Starting GDB ImageWatch configuration for Ubuntu 16.04."
+git submodule init
+git submodule update
 set -e
 sudo apt-get -y install libpython3-dev
 mkdir build
