43cb133d7298abe058550e8e52e4aa7a8eb3f823
Bruno Rosa <bruno.rosa@navinfo.eu>
2019-09-05 10:17:55 +0200

    Update copyright notice year

--- src/io/buffer_exporter.cpp
+++ src/io/buffer_exporter.cpp
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2017 OpenImageDebugger contributors
+ * Copyright (c) 2015-2019 OpenImageDebugger contributors
  * (https://github.com/OpenImageDebugger/OpenImageDebugger)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
--- src/oidbridge/oid_bridge.h
+++ src/oidbridge/oid_bridge.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2017 OpenImageDebugger
+ * Copyright (c) 2015-2019 OpenImageDebugger
  * (https://github.com/OpenImageDebugger/OpenImageDebugger)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
