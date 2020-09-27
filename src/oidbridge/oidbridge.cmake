74663841b5bdb4991073a9fcfa7c7007dd7fd443
Bruno Rosa <1791393+brunoalr@users.noreply.github.com>
2020-09-27 20:32:28 +0200

    Initial version of oidbridge.cmake

---
+++ src/oidbridge/oidbridge.cmake
@@ -0,0 +1,30 @@
+cmake_minimum_required(VERSION 3.10.0)
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/../../../commom.cmake)
+
+SET(SOURCES
+    ../oid_bridge.cpp
+    ../../debuggerinterface/python_native_interface.cpp
+    ../../ipc/message_exchange.cpp
+    ../../ipc/raw_data_decode.cpp)
+
+
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}
+                    ${CMAKE_CURRENT_SOURCE_DIR}/../..)
+
+# # Prevent strip from producing spurious error messages
+# QMAKE_STRIP = echo "strip disabled: "
+
+# TEMPLATE = lib
+add_compile_options("$<$<PLATFORM_ID:UNIX>:-Wl,--exclude-libs,ALL>")
+
+
+# # Deployment settings
+# macx {
+#     BRIDGE_INSTALL_FOLDER = $$PREFIX/OpenImageDebugger/oidwindow.app/Contents/MacOS
+# }
+# linux {
+#     BRIDGE_INSTALL_FOLDER = $$PREFIX/OpenImageDebugger/
+# }
+
+# include($$PWD/../../resources/deployscripts.pri)
