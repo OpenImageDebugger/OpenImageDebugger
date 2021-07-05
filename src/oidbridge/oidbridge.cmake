99414be3122f4775e29adb45825113e9e7db1f1a
Bruno Rosa <1791393+brunoalr@users.noreply.github.com>
2021-07-05 10:28:36 +0200

    Remove duplicate CMake code
    Issue: #31

--- src/oidbridge/oidbridge.cmake
+++ src/oidbridge/oidbridge.cmake
@@ -9,16 +9,27 @@ SET(SOURCES
     ../../system/process/process_unix.cpp
     ../../system/process/process_win32.cpp)
 
-
-include_directories(${CMAKE_CURRENT_SOURCE_DIR}
-                    ${CMAKE_CURRENT_SOURCE_DIR}/../..)
+add_library(${PROJECT_NAME} SHARED ${SOURCES})
 
 add_compile_options("$<$<PLATFORM_ID:UNIX>:-Wl,--exclude-libs,ALL>")
+
+target_include_directories(${PROJECT_NAME}
+                           PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/../..)
+
+target_include_directories(${PROJECT_NAME} SYSTEM
+                           PRIVATE ${PYTHON_INCLUDE_DIR})
+
+target_link_libraries(${PROJECT_NAME} PRIVATE
+                      Qt5::Core
+                      Qt5::Network
+                      Threads::Threads
+                      ${PYTHON_LIBRARY})
+
+install(TARGETS ${PROJECT_NAME} DESTINATION OpenImageDebugger)
 
 # Need to unset these vars to trigger find_package with a different required version
 unset(PYTHON_INCLUDE_DIR CACHE)
 unset(PYTHON_LIBRARY CACHE)
 
 # TODO: deal with macos installation
-
 include(${CMAKE_CURRENT_SOURCE_DIR}/../../../resources/deployscripts.cmake)
