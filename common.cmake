1b483925425bbea54111b37a68a6bfb837b1faba
Anton Hromov <a.gromov@abtollc.com>
2021-11-26 09:33:02 +0200

    Add check if CMAKE_BUILD_TYPE and CMAKE_INSTALL_PREFIX are defined or not
    See more details here:
    https://stackoverflow.com/a/39485990
    https://stackoverflow.com/a/48832234

--- common.cmake
+++ common.cmake
@@ -23,8 +23,13 @@
 
 cmake_minimum_required(VERSION 3.1.0)
 
-set(CMAKE_BUILD_TYPE Release CACHE STRING "Build type")
-set(CMAKE_INSTALL_PREFIX /usr/local/bin CACHE PATH "Install path")
+if (NOT CMAKE_BUILD_TYPE OR CMAKE_BUILD_TYPE STREQUAL "")
+    set(CMAKE_BUILD_TYPE Release CACHE STRING "Build type" FORCE)
+endif()
+
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+    set(CMAKE_INSTALL_PREFIX /usr/local/bin CACHE PATH "Install path" FORCE)
+endif()
 
 # We want to be as strict as possible with the standards
 set(CMAKE_CXX_STANDARD 14)
