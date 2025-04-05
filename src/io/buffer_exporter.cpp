517cfb9c2f19a02fb33ae6dbac3b8347ec090264
Bruno Alexandre Rosa <1791393+brunoalr@users.noreply.github.com>
2025-04-05 17:05:15 -0300

    feat: module interface unit

--- src/io/buffer_exporter.cpp
+++ src/io/buffer_exporter.cpp
@@ -34,14 +34,13 @@ module;
 
 #include <QPixmap>
 
-#include "visualization/components/buffer.h"
 #include "ipc/raw_data_decode.h"
+#include "visualization/components/buffer.h"
 
-export module BufferExporter;
+module BufferExporter;
 
 namespace oid::BufferExporter
 {
-export enum class OutputType { Bitmap, OctaveMatrix };
 
 template <typename T>
 float get_multiplier()
@@ -241,9 +240,9 @@ void export_binary(const char* fname, const Buffer* buffer)
 }
 
 
-export void export_buffer(const Buffer* buffer,
-                          const std::string& path,
-                          const OutputType type)
+void export_buffer(const Buffer* buffer,
+                   const std::string& path,
+                   const OutputType type)
 {
     if (type == OutputType::Bitmap) {
         switch (buffer->type) {
@@ -289,4 +288,4 @@ export void export_buffer(const Buffer* buffer,
     }
 }
 
-} // namespace BufferExporter
+} // namespace oid::BufferExporter
